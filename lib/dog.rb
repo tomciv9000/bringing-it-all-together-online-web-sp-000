d2f856609662075a1317a9f66fa10553b48c43eb
Tom White <eugene.white@gmail.com>
2019-08-07 17:14:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,8 +69,8 @@ attr_accessor :name, :breed, :id
   end
   
   def self.find_by_name(name)
-    sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end
   
