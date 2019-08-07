cb31dcea795ff0f91d68807fe885453261e403de
Tom White <eugene.white@gmail.com>
2019-08-07 16:54:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,9 @@ attr_accessor :name, :breed, :id
   end
   
   def self find_by_id(id)
-    
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Song.new(result[0], result[1], result[2])
   end
   
 end
