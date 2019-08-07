b39bf35d8d60df8679da0d1f1e5d262f8a5e7c74
Tom White <eugene.white@gmail.com>
2019-08-07 17:04:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,8 @@ attr_accessor :name, :breed, :id
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
     Dog.new(id: result[0], name: result[1], breed: result[2])
-
   end
+  
+  def self.find_or_create_by
   
 end
